71c55e3b6325cbecfd50e8e0d2d93b87701f9ba5
unknown <devanshraghav2000@gmail.com>
2024-02-04 22:49:52 +0530

    added category tab

--- src/components/Body/Button.js
+++ src/components/Body/Button.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const Button = (props) => {
+  const {category,active,setActiveCategory} = props;
+  const {categoryName,categoryId} = category;
+  const handler = () =>{
+    setActiveCategory(categoryId);
+    
+  }
+
   return (
     <div>
-        <button className='bg-gray-200 px-3 py-1 m-4 rounded-md'>{props.name}</button>
+      <button className={active===categoryId? "bg-black px-3 py-1 m-4 rounded-md text-white" : "bg-gray-200 px-3 py-1 m-4 rounded-md"} onClick={handler}>
+        {categoryName}
+      </button>
     </div>
-  )
-}
+  );
+};
 
 export default Button;
--- src/components/Body/ButtonList.js
+++ src/components/Body/ButtonList.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from './Button';
 
-const list = ["All", "Music", "Movies", "Gaming", "Mixes", "BGMI","React", "Live", "Web Development", "Javascript","Lo-fi"];
+const list = [
+  {categoryId:0,categoryName:"All"},
+  {categoryId:10,categoryName:"Music"},
+  {categoryId:20,categoryName:"Gaming"},
+  {categoryId:17,categoryName:"Sports"},
+  {categoryId:15,categoryName:"Animals"},
+  {categoryId:30,categoryName:"Movies"},
+  {categoryId:31,categoryName:"Anime"},
+  {categoryId:34,categoryName:"Comedy"},
+  {categoryId:39,categoryName:"Horror"},
+  {categoryId:42,categoryName:"Shorts"},
+  {categoryId:44,categoryName:"Trailers"},
+  // {categoryId:43,categoryName:"Shows"},
+  // {categoryId:41,categoryName:"Thriller"},
+  // {categoryId:40,categoryName:"Sci-fi"},
+  // {categoryId:36,categoryName:"Drama"},
+  // {categoryId:35,categoryName:"Documentary"},
+  // {categoryId:32,categoryName:"Actiom/Adventure"},
+  // {categoryId:27,categoryName:"Education"},
+  // {categoryId:22,categoryName:"Peopls&Blogs"},
+  // {categoryId:25,categoryName:"News"},
+  // {categoryId:21,categoryName:"VideBlogging"},
+]
+
 const ButtonList = () => {
+  const [activeCategory, setActiveCategory] = useState(0);
+
+
+
   return (
-    <div className='flex'>
+    <div className='flex flex-wrap'>
       {
-        list.map((button,index)=><Button name={button} key={index}/>)
+        list.map((category,index)=><Button category={category} key={index} active={activeCategory} setActiveCategory={setActiveCategory}/>)
       }
     </div>
   )
